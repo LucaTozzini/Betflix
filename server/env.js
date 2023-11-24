d4c0bf4cc84c5f1ea8f8926fbd71131e817235f1
Luca Tozzini <46906302+LucaTozzini@users.noreply.github.com>
2023-11-24 03:11:29 -0800

    Create env.js

---
+++ server/env.js
@@ -0,0 +1,13 @@
+const env = {
+    databasePath: '',
+    moviesPath: '',
+    showsPath: '',
+    userImagesPath: '',
+    subtitlesPath: '',
+    TMDB_KEY: '',
+    OpenSubtitles_KEY: '',
+    OpenSubtitles_username: '',
+    OpenSubtitles_password: '',
+}
+
+export default env;
